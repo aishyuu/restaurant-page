a13c1f362fa057fbf341a68006f8a20bd37b7c85
aishyuu <ivanmatias1331@gmail.com>
2023-02-09 21:52:11 -0800

    Added hard coded html to file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import './style.css'
 
 function component(name) {
-    const element = document.createElement('div');
-    element.classList.add('header');
-    element.innerHTML = `Hello ${name}`;
-
-    return element;
+    
 }
 
-document.body.appendChild(component("there"));
+document.body.appendChild(component("Ivan"));
